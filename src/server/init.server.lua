bef79c86cabf21fb6e27d71a26590eb3ddae5167
PolyVector <PolyVector@protonmail.com>
2023-12-30 02:43:23 +0000

    Add skeleton for service creation, test server creation with server scripts

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,11 @@
-print("Hello world, from server!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Silicon = require(ReplicatedStorage.Shared.Silicon)
+
+local Service = Silicon.Service
+local Implements = Silicon.Implements
+
+Service("TestService")(Implements.OnStart())({
+	OnStart = function()
+		print("Test")
+	end,
+})
