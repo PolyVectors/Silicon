af001feaae196957543e248059afe08b44804df0
PolyVector <PolyVector@protonmail.com>
2024-01-05 20:59:34 +0000

    Built and published by Moonwave

---
+++ assets/js/6d525e5c.3e1fa644.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[201],{54091:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2},{"value":".Controller","id":"Controller","level":3},{"value":".Service","id":"Service","level":3}]')}}]);
